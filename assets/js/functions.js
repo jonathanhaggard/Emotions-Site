bde320f65d39eeec3165099a0bec8fe514c08605
Jonathan Haggard <info@jonathanhaggard.com>
2019-10-21 14:19:56 -0700

    initial prototype

---
+++ assets/js/functions.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+
+  $( ".emotion" ).each(function( index ) {
+
+
+
+    console.log( index + ": " + $( this ).text() );
+
+    $(this).append("<div class=" + "card" + "></div>");
+
+    $(".card").each(function (index){
+      $(this).attr('id', index );
+
+
+    });
+
+
+
+    lottie.loadAnimation({
+      container: document.getElementById(index), // the dom element that will contain the animation
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'anim/'+index+'.json' // the path to the animation json
+    });
+
+
+  });
+
+  $(document).on('mousemove', function(e){
+    $('.card').css({
+       left:  e.pageX,
+       top:   e.pageY
+    });
+  });
+
+
+
+});
